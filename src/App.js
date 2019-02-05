7db407c47d87689bcf22c8ac52741caffceb12b4
Katy Gibson <klgibson528@gmail.com>
2019-02-05 12:59:51 -0600

    part 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'normalize.css';
 import './App.css';
+import Card from './Card.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Memory Game
+          </h1>
+          <h3>Match Cards to Win!</h3>
         </header>
+        <div><Card /><Card /><Card /><Card /></div>
+        <div><Card /><Card /><Card /><Card /></div>
+        <div><Card /><Card /><Card /><Card /></div>
+        <div><Card /><Card /><Card /><Card /></div>
+
       </div>
     );
   }
---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+class Card extends Component {
+    render(){
+        return <div className="MemoryCard"><img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="DigitalCrafts Logo"/></div>
+    }
+
+}
+
+export default Card;
