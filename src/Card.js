7007f4040ed5bcd63a41daf90acbde3b48054926
Katy Gibson <klgibson528@gmail.com>
2019-02-05 14:37:13 -0600

    part 2 finished

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import './Card.css';
 
 class Card extends Component {
-    render(){
-        return <div className="MemoryCard"><img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="DigitalCrafts Logo"/></div>
+    constructor(props) {
+        super(props);
+        this.state = { isFlipped: false }
+    }
+
+    clickHandler() {
+        this.setState({ isFlipped: !this.state.isFlipped })
+        console.log('clicked')
+    }
+
+    render() {
+        let memoryCardInnerClass = "MemoryCardInner"
+        if(this.state.isFlipped === true){
+            memoryCardInnerClass += " flipped"
+        }
+        return (
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack"><img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="DigitalCrafts Logo" /></div>
+                    <div className="MemoryCardFront">∆</div>
+                </div>
+            </div>
+        )
     }
 
 }
