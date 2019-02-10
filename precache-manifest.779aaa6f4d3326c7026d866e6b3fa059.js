13698b2f8151b46722f212f4b6467a7de92cdfdc
Katy Gibson <klgibson528@gmail.com>
2019-02-10 08:21:21 -0600

    Updates

---
+++ precache-manifest.779aaa6f4d3326c7026d866e6b3fa059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "6245d4a95a26009e5135",
+    "url": "/react-memory-game/static/js/main.6245d4a9.chunk.js"
+  },
+  {
+    "revision": "9d4760d5457b938d61d8",
+    "url": "/react-memory-game/static/js/1.9d4760d5.chunk.js"
+  },
+  {
+    "revision": "6245d4a95a26009e5135",
+    "url": "/react-memory-game/static/css/main.52bd7c78.chunk.css"
+  },
+  {
+    "revision": "9d4760d5457b938d61d8",
+    "url": "/react-memory-game/static/css/1.a0bfa417.chunk.css"
+  },
+  {
+    "revision": "c051e647a56287a130f2a7829c9edb0a",
+    "url": "/react-memory-game/index.html"
+  }
+];
